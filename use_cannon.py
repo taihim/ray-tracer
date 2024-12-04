3fc38dcc1fb3a57180f03eba3ce3da0219661bc6
taihim <tai.him18@gmail.com>
2024-12-05 00:05:03 +0500

    feat: canvas demo script prepared

--- use_cannon.py
+++ use_cannon.py
@@ -1,18 +1,45 @@
-from src.ray_tracer import Canvas
+from src.cannon import Environment, Projectile, tick
+from src.ray_tracer import Canvas, ColorTuple, CustomTuple
 
-# # p1 = CustomTuple.point(0, 1, 0)
-# # v1 = CustomTuple.vector(1, 1, 0)
+cv = Canvas(550, 900, (0, 0, 0))
+p1 = CustomTuple.point(0, 1, 0)
+p2 = CustomTuple.point(1, 0.5, 0)
+v1 = CustomTuple.vector(1, 1.8, 0).normalize() * 11.25
+v2 = CustomTuple.vector(1, 1, 0).normalize() * 15.25
+c1 = ColorTuple(0, 0, 0)
 
-# # gravity = CustomTuple.vector(0, -0.1, 0)
-# # wind = CustomTuple.vector(-0.01, 0, 0)
+gravity = CustomTuple.vector(0, -0.1, 0)
+wind = CustomTuple.vector(-0.01, 0, 0)
 
-# # projectile = Projectile(p1, v1)
-# # environment = Environment(gravity, wind)
+projectile = Projectile(p1, v1)
+projectile2 = Projectile(p2, v2)
 
-# # while projectile.position.y > 0:
-# #     projectile = tick(environment, projectile)
-# #     print(projectile.position)
+environment = Environment(gravity, wind)
 
+
+print(cv.width)
+print(cv.height)
+print(projectile.position)
+cv.pixels[-(int(projectile.position.y) + 1)][int(projectile.position.x)] = ColorTuple(1, 1, 1)
+while projectile.position.y > 0:
+    projectile = tick(environment, projectile)
+    if int(projectile.position.x) >= cv.width or int(projectile.position.y) >= cv.height:
+        print("yes", projectile.position, cv.width, cv.height)
+        continue
+    else:
+        cv.pixels[-(int(projectile.position.y + 1))][int(projectile.position.x)] = ColorTuple(1, 1, 1)
+
+while projectile2.position.y > 0:
+    projectile2 = tick(environment, projectile2)
+    if int(projectile2.position.x) >= cv.width or int(projectile2.position.y) >= cv.height:
+        print("yes", projectile2.position, cv.width, cv.height)
+        continue
+    else:
+        cv.pixels[-(int(projectile2.position.y + 1))][int(projectile2.position.x)] = ColorTuple(1, 1, 1)
+
+
+data = cv.canvas_to_ppm()
+cv.save(data=data)
 # # c1 = ColorTuple(1, 2, 3)
 # # c2 = ColorTuple(4, 5, 6)
 
@@ -24,19 +51,8 @@ from src.ray_tracer import Canvas
 # # print(c4 == ColorTuple(3, 6, 9))
 # # print(c1.hadamard(c2))
 
-cv = Canvas(5, 7, (0.5, 0.3, 0.7))
-
 # cv[0][0] = ColorTuple(1.5, 0, 0)
 # cv[1][2] = ColorTuple(0, 0.5, 0)
 # cv[2][4] = ColorTuple(-0.5, 0, 1)
-data = cv.canvas_to_ppm()
-data_split = data.split("\n")
-print(data)
-print(data_)
+# data = cv.canvas_to_ppm()
 # cv.save(data=data)
-
-# for i in range(cv.height):
-#     for j in range(cv.width):
-#         print(cv.pixels[i][j])
-#         print(c1)
-#         print(cv.pixels[i][j] == c1)
