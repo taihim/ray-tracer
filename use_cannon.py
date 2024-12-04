37c7e54a010fa9ec7da8af1329e434decec2359a
taihim <tai.him18@gmail.com>
2024-12-05 00:05:26 +0500

    feat: canvas demo updated

--- use_cannon.py
+++ use_cannon.py
@@ -40,19 +40,3 @@ while projectile2.position.y > 0:
 
 data = cv.canvas_to_ppm()
 cv.save(data=data)
-# # c1 = ColorTuple(1, 2, 3)
-# # c2 = ColorTuple(4, 5, 6)
-
-# # # print(c1)
-
-# # # print(c1 + c2)
-# # # print(c1 - c2)
-# # c4 = c1 * 3
-# # print(c4 == ColorTuple(3, 6, 9))
-# # print(c1.hadamard(c2))
-
-# cv[0][0] = ColorTuple(1.5, 0, 0)
-# cv[1][2] = ColorTuple(0, 0.5, 0)
-# cv[2][4] = ColorTuple(-0.5, 0, 1)
-# data = cv.canvas_to_ppm()
-# cv.save(data=data)
