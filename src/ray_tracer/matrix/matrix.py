36324e6e189b54b8fc6bf221e724505f07ded045
taihim <tai.him18@gmail.com>
2024-12-11 02:47:58 +0500

    feat: added matrix __eq__ method and tests.

--- src/ray_tracer/matrix/matrix.py
+++ src/ray_tracer/matrix/matrix.py
@@ -1,11 +1,15 @@
 from typing import overload
 
+from src.ray_tracer.utils import compare_float
+
 
 class RTMatrix:
     """Class representing matrices used in the Raytracer."""
 
     def __init__(self, rows: int = 1, cols: int = 1, matrix: list[list[float]] | None = None) -> None:
         if matrix:
+            if not isinstance(matrix, list) or not isinstance(matrix[0], list):
+                raise TypeError(f"Expected both rows and cols to be lists, got {(type(matrix), type(matrix[0]))}.")
             self.rows = len(matrix)
             self.cols = len(matrix[0])
             self.data = matrix
@@ -42,3 +46,18 @@ class RTMatrix:
                 raise ValueError(f"Expected {self.cols} columns, got {len(value)}.")
         else:
             raise TypeError(f"Expected index of type Int, got {type(index)}.")
+
+    def __eq__(self, m2: object) -> bool:
+        """Compares two matrices."""
+        if not isinstance(m2, RTMatrix):
+            raise NotImplementedError
+
+        if m2.rows != self.rows or m2.cols != self.cols:
+            raise ValueError("Can only compare matrices with the same dimensions.")
+
+        for r_idx, row in enumerate(self.data):
+            for col_idx, col in enumerate(row):
+                if not compare_float(col, m2[r_idx][col_idx]):
+                    return False
+
+        return True
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,7 +1,30 @@
 from src.ray_tracer.matrix import RTMatrix
 
 
-def test_matrix_init() -> None:
+def test_matrix_init_1x1() -> None:
+    m1 = RTMatrix(matrix=[[-3], [1]])
+
+    assert m1[0][0] == -3
+    assert m1[1][0] == 1
+
+
+def test_matrix_init_2x2() -> None:
+    m1 = RTMatrix(matrix=[[-3, 5], [1, -2]])
+
+    assert m1[0][0] == -3
+    assert m1[0][1] == 5
+    assert m1[1][0] == 1
+    assert m1[1][1] == -2
+
+
+def test_matrix_init_3x3() -> None:
+    m1 = RTMatrix(matrix=[[-3, 5, 0], [1, -2, -7], [0, 1, 1]])
+    assert m1[0][0] == -3
+    assert m1[1][1] == -2
+    assert m1[2][2] == 1
+
+
+def test_matrix_init_4x4() -> None:
     m1 = RTMatrix(matrix=[[1, 2, 3, 4], [5.5, 6.5, 7.5, 8.5], [9, 10, 11, 12], [13.5, 14.5, 15.5, 16.5]])
 
     assert m1[0][0] == 1
@@ -11,3 +34,58 @@ def test_matrix_init() -> None:
     assert m1[2][2] == 11
     assert m1[3][0] == 13.5
     assert m1[3][2] == 15.5
+
+def test_matrices_equal_1x1() -> None:
+    m1 = RTMatrix(matrix=[[-3]])
+    m2 = RTMatrix(matrix=[[-3]])
+
+    assert m1 == m2
+
+
+def test_matrices_equal_2x2() -> None:
+    m1 = RTMatrix(matrix=[[-3], [1]])
+    m2 = RTMatrix(matrix=[[-3], [1]])
+
+    assert m1 == m2
+
+
+def test_matrices_equal_3x3() -> None:
+    m1 = RTMatrix(matrix=[[-3, 5, 0], [1, -2, -7], [0, 1, 1]])
+    m2 = RTMatrix(matrix=[[-3, 5, 0], [1, -2, -7], [0, 1, 1]])
+
+    assert m1 == m2
+
+
+def test_matrices_equal_4x4() -> None:
+    m1 = RTMatrix(matrix=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
+    m2 = RTMatrix(matrix=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
+
+    assert m1 == m2
+
+
+def test_matrices_unequal_1x1() -> None:
+    m1 = RTMatrix(matrix=[[-3], [1]])
+    m2 = RTMatrix(matrix=[[-3], [12]])
+
+    assert m1 != m2
+
+
+def test_matrices_unequal_2x2() -> None:
+    m1 = RTMatrix(matrix=[[-3, 5], [1, -2]])
+    m2 = RTMatrix(matrix=[[-3, 5], [12, -2]])
+
+    assert m1 != m2
+
+
+def test_matrices_unequal_3x3() -> None:
+    m1 = RTMatrix(matrix=[[-3, 5, 0], [1, -2, -7], [0, 1, 1]])
+    m2 = RTMatrix(matrix=[[-3, 5, 0], [11, -2, -7], [0, 1, 1]])
+
+    assert m1 != m2
+
+
+def test_matrices_unequal_4x4() -> None:
+    m1 = RTMatrix(matrix=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
+    m2 = RTMatrix(matrix=[[2, 3, 4, 5], [6, 7, 8, 9], [8, 7, 6, 5], [4, 3, 2, 1]])
+
+    assert m1 != m2
