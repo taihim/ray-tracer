d65e039396e7381f30aae04c8a6315691a5f6c6f
taihim <tai.him18@gmail.com>
2025-01-26 08:50:34 +0100

    chore: added scratchpad file for testing and some pdm commands

---
+++ e2e/scratchpad.py
@@ -0,0 +1,5 @@
+from src.ray_tracer import RTMatrix
+
+m1 = RTMatrix(matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print(m1)
--- src/ray_tracer/__init__.py
+++ src/ray_tracer/__init__.py
@@ -1,4 +1,5 @@
 from .canvas import Canvas
+from .matrix import RTMatrix
 from .tuples import ColorTuple, CustomTuple
 
-__all__ = ["Canvas", "ColorTuple", "CustomTuple"]
+__all__ = ["Canvas", "ColorTuple", "CustomTuple", "RTMatrix"]
