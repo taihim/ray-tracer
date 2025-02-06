21b7a68b5d3dea4ae5272093805cac2c60076793
taihim <tai.him18@gmail.com>
2025-02-06 22:03:08 +0100

    feat: added scaling transform + tests

--- src/ray_tracer/matrix/transforms.py
+++ src/ray_tracer/matrix/transforms.py
@@ -1,11 +1,21 @@
 from src.ray_tracer.matrix import RTMatrix
 
 
-def translation(x: float, y: float, z: float) -> RTMatrix:
+def translate(x: float, y: float, z: float) -> RTMatrix:
     """Create and return a translation matrix."""
-    t_matrix = RTMatrix.identity()
-    t_matrix[0][3] = x
-    t_matrix[1][3] = y
-    t_matrix[2][3] = z
+    translation_matrix = RTMatrix.identity()
+    translation_matrix[0][3] = x
+    translation_matrix[1][3] = y
+    translation_matrix[2][3] = z
 
-    return t_matrix
+    return translation_matrix
+
+
+def scale(x: float, y: float, z: float) -> RTMatrix:
+    """Create and return a scaling matrix."""
+    scaling_matrix = RTMatrix.identity()
+    scaling_matrix[0][0] = x
+    scaling_matrix[1][1] = y
+    scaling_matrix[2][2] = z
+
+    return scaling_matrix
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,9 +1,9 @@
 from src.ray_tracer import CustomTuple
-from src.ray_tracer.matrix.transforms import translation
+from src.ray_tracer.matrix.transforms import scale, translate
 
 
 def test_translation() -> None:
-    t1 = translation(5, -3, 2)
+    t1 = translate(5, -3, 2)
 
     p1 = CustomTuple.point(-3, 4, 5)
     v1 = CustomTuple.vector(-3, 4, 5)
@@ -13,7 +13,7 @@ def test_translation() -> None:
 
 
 def test_translation_inverse() -> None:
-    t1 = translation(5, -3, 2)
+    t1 = translate(5, -3, 2)
     t1_inv = t1.inverse()
 
     p1 = CustomTuple.point(-3, 4, 5)
@@ -21,3 +21,36 @@ def test_translation_inverse() -> None:
 
     assert t1_inv * p1 == CustomTuple.point(-8, 7, 3)
     assert t1 * v1 == v1
+
+
+def test_scaling() -> None:
+    t1 = scale(2, 3, 4)
+
+    p1 = CustomTuple.point(-4, 6, 8)
+    v1 = CustomTuple.vector(-4, 6, 8)
+
+    assert t1 * p1 == CustomTuple.point(-8, 18, 32)
+    assert t1 * v1 == CustomTuple.vector(-8, 18, 32)
+
+
+def test_scaling_inverse() -> None:
+    t1 = scale(2, 3, 4)
+    t1_inv = t1.inverse()
+
+    p1 = CustomTuple.point(-4, 6, 8)
+    v1 = CustomTuple.vector(-4, 6, 8)
+
+    assert t1_inv * p1 == CustomTuple.point(-2, 2, 2)
+    assert t1_inv * v1 == CustomTuple.vector(-2, 2, 2)
+
+
+def test_reflection() -> None:
+    # reflection just means multiplying by -1
+
+    t1 = scale(-1, 1, 1)
+
+    p1 = CustomTuple.point(2, 3, 4)
+    v1 = CustomTuple.vector(2, 3, 4)
+
+    assert t1 * p1 == CustomTuple.point(-2, 3, 4)
+    assert t1 * v1 == CustomTuple.vector(-2, 3, 4)
