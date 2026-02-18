300acb136e2e7f02d6db748f4d6bb364faefd0df
Taimur Ibrahim <tai.him18@gmail.com>
2026-02-18 22:01:52 +0100

    update lighting normalization

--- src/ray_tracer/lights/lighting.py
+++ src/ray_tracer/lights/lighting.py
@@ -1,3 +1,5 @@
+from numpy.linalg import norm
+
 from src.ray_tracer.lights.point import PointLight
 from src.ray_tracer.material import Material
 from src.ray_tracer.tuples import ColorTuple, CustomTuple
@@ -10,4 +12,21 @@ def lighting(
     effective_color = m.color * light.intensity
     light_vec = (light.position - position).normalize()
 
-    return ColorTuple(1, 2, 3)
+    ambient = effective_color * m.ambient
+
+    light_dot_normal = light_vec.dot(normal_vec)
+    if light_dot_normal < 0:
+        diffuse = ColorTuple(0, 0, 0)
+        specular = ColorTuple(0, 0, 0)
+    else:
+        diffuse = effective_color * m.diffuse * light_dot_normal
+        reflect_vec = -light_vec.reflect(normal_vec)
+        reflect_dot_eye = reflect_vec.dot(eye_vec)
+
+        if reflect_dot_eye <= 0:
+            specular = ColorTuple(0, 0, 0)
+        else:
+            factor = pow(reflect_dot_eye, m.shininess)
+            specular = light.intensity * m.specular * factor
+
+    return ambient + diffuse + specular
