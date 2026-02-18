a66d61990ab44c90ac39000d38b3f15f1399f308
Taimur Ibrahim <tai.him18@gmail.com>
2026-02-18 22:10:53 +0100

    lighting coverage completed

--- tests/test_lighting.py
+++ tests/test_lighting.py
@@ -57,6 +57,19 @@ def test_lighting_eye_in_path_of_reflection():
     assert result == ColorTuple(1.6364, 1.6364, 1.6364)
 
 
+def test_lighting_reflection_away_from_eye():
+    """Light is in front of surface, reflection vector points away from eye"""
+    m = Material(ColorTuple(1, 1, 1), 0.1, 0.9, 0.9, 200.0)
+    position = CustomTuple(0, 0, 0, 1)
+
+    eye_vec = CustomTuple(0, 0, 1)
+    normal_vec = CustomTuple(0, 0, -1)
+    light = PointLight(CustomTuple(0, 0, -10, 1), ColorTuple(1, 1, 1))
+
+    result = lighting(m, light, position, eye_vec, normal_vec)
+    assert result == ColorTuple(1.0, 1.0, 1.0)
+
+
 def test_lighting_light_behind_surface():
     """Light is at a 45 degree angle to surface and eye is in the path of the reflection"""
     m = Material(ColorTuple(1, 1, 1), 0.1, 0.9, 0.9, 200.0)
