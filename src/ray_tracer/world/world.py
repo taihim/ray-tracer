40cfc07ecf70a0f0c1f3ed2d10aa51ec5ffd7d3c
Taimur Ibrahim <tai.him18@gmail.com>
2026-02-24 21:13:01 +0100

    init world intersect function

--- src/ray_tracer/world/world.py
+++ src/ray_tracer/world/world.py
@@ -1,5 +1,13 @@
 class World:
     """World class for the ray tracer"""
 
-    def __init__():
+    def __init__(self, light, objects):
         pass
+
+
+
+def intersect_world(world, ray):
+    """Return intersections for a ray and all objects in a world object."""
+    pass
+
+    
