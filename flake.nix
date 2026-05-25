10f2e9ec0485a4c3b3534a3a4b9fe7d2efb9ad7e
Taimur Ibrahim <tai.him18@gmail.com>
2026-05-25 21:39:32 +0200

    add nixos flakes

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Development shell for ray-tracer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python312
+              pkgs.uv
+            ];
+
+            env = {
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+                pkgs.stdenv.cc.cc.lib
+              ];
+              UV_NO_MANAGED_PYTHON = "1";
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+          };
+        }
+      );
+    };
+}
